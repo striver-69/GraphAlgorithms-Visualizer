f449fe7777efa234320e63a9233bea8470158a4c
Chiranjivee Saksham <75401112+striver-69@users.noreply.github.com>
2021-08-31 21:14:00 +0530

    Update navbar.js

--- src/pathfindingVisualizer/navbar.js
+++ src/pathfindingVisualizer/navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './navbar.css';
-const brand = window.innerWidth > 600 ? 'Pathfinding Visualizer' : 'Pathfinder';
+const brand = 'GraphSaga';
 
 export class Navbar extends Component {
   state = {
